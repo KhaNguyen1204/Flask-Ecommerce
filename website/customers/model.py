abc023d894a6ef0f274eb5f6b62c9f549c2ab28d
K97 <phamanhkiet97123@gmail.com>
2025-05-12 14:47:27 +0700

    Full base code for website

--- website/customers/model.py
+++ website/customers/model.py
@@ -1,12 +1,13 @@
 from website import db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
+import json
 
 @login_manager.user_loader
 def user_loader(user_id):
     return Register.query.get(int(user_id))
 
-class Register(db.Model):
+class Register(db.Model, UserMixin):
     __tablename__ = 'customers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=False, nullable=False)
@@ -39,3 +40,29 @@ class Register(db.Model):
 
     def get_id(self):
         return str(self.id)
+
+class JsonEncodedDict(db.TypeDecorator):
+    impl = db.Text
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return '{}'
+        else:
+            return json.dumps(value)
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return {}
+        else:
+            return json.loads(value)
+
+class CustomerOrder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    invoice = db.Column(db.String(100), unique=True, nullable=False)
+    status = db.Column(db.String(100), unique=False, nullable=False, default='Pending')
+    customer_id = db.Column(db.Integer, unique=False, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    orders = db.Column(JsonEncodedDict)
+
+    def __repr__(self):
+        return '<CustomerOrder %r>' % self.invoice
--- website/customers/routes.py
+++ website/customers/routes.py
@@ -1,9 +1,10 @@
-from flask import redirect, render_template, url_for, flash, request, session, current_app
+from flask import redirect, render_template, url_for, flash, request, session, current_app, make_response
 from website import app, db, photos, search, bcrypt
 from .forms import CustomerRegisterForm, CustomerLoginForm
-from .model import Register
+from .model import Register, CustomerOrder
 import os
 import secrets
+import pdfkit
 from flask_login import login_required, current_user, logout_user, login_user
 
 @app.route('/customer/register', methods=['GET', 'POST'])
@@ -35,7 +36,78 @@ def customer_login():
 
     return render_template('customers/login.html', form=form)
 
-# @app.route('customer/logout')
-# def customer_logout():
-#     logout_user()
-#     return redirect(url_for('customer_login'))
+@app.route('/customer/logout')
+def customer_logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/getorder')
+@login_required
+def get_order():
+    if current_user.is_authenticated:
+        customer_id = current_user.id
+        invoice = secrets.token_hex(5)
+        try:
+            order = CustomerOrder(invoice=invoice, customer_id=customer_id, orders=session['ShopCart'])
+            db.session.add(order)
+            db.session.commit()
+            session.pop('ShopCart')
+            flash(f'Your order {order.invoice} has been sent success', 'success')
+            return redirect(url_for('orders', invoice=invoice))
+        except Exception as e:
+            print(e)
+            flash('Error occurred while processing your order', 'danger')
+            return redirect(url_for('getCart'))
+
+@app.route('/orders/<invoice>')
+@login_required
+def orders(invoice):
+    if current_user.is_authenticated:
+        grand_total = 0
+        sub_total = 0
+        customer_id = current_user.id
+        customer = Register.query.filter_by(id=customer_id).first()
+        orders = CustomerOrder.query.filter_by(customer_id=customer_id).order_by(CustomerOrder.id.desc()).first()
+        for _key, product in orders.orders.items():
+            discount = (product['discount'] / 100) * float(product['price'])
+            sub_total += float(product['price']) * int(product['quantity'])
+            sub_total -= discount
+            tax = ("%.2f" % (.6 *float(sub_total)))
+            grand_total += float("%.2f" % (1.06 * sub_total))
+    else:
+        return redirect(url_for('customer_login'))
+    return render_template('customers/orders.html', customer=customer, orders=orders, grand_total=grand_total,sub_total=sub_total, tax=tax, invoice=invoice)
+
+import pdfkit
+
+@app.route('/getpdf/<invoice>', methods=['POST'])
+@login_required
+def get_pdf(invoice):
+    if current_user.is_authenticated:
+        grand_total = 0
+        sub_total = 0
+        customer_id = current_user.id
+        if request.method == 'POST':
+            customer = Register.query.filter_by(id=customer_id).first()
+            orders = CustomerOrder.query.filter_by(customer_id=customer_id).order_by(CustomerOrder.id.desc()).first()
+            for _key, product in orders.orders.items():
+                discount = (product['discount'] / 100) * float(product['price'])
+                sub_total += float(product['price']) * int(product['quantity'])
+                sub_total -= discount
+                tax = ("%.2f" % (.6 * float(sub_total)))
+                grand_total += float("%.2f" % (1.06 * sub_total))
+            rendered = render_template('customers/pdf.html', customer=customer, orders=orders, grand_total=grand_total, tax=tax, invoice=invoice)
+            try:
+                options = {
+                    'enable-local-file-access': None
+                }
+                pdf = pdfkit.from_string(rendered, False, options=options)
+                response = make_response(pdf)
+                response.headers['Content-Type'] = 'application/pdf'
+                response.headers['Content-Disposition'] = f'atteched; filename={invoice}.pdf'
+                return response
+            except OSError as e:
+                print({'error': str(e)})
+                flash('Lỗi khi tạo PDF. Vui lòng thử lại.', 'danger')
+                return redirect(url_for('orders', invoice=invoice))
+    return redirect(url_for('customer_login'))
